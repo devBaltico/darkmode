302f7b8af1b646c9b2d211fd04fd04589b8688ad
Luis Alberto Roa <devbaltico@gmail.com>
2023-09-28 10:58:22 -0400

    Mantener la configuracion en el localstorage

--- js/main.js
+++ js/main.js
@@ -3,4 +3,22 @@ const btnswitch = document.querySelector('#switch');
 btnswitch.addEventListener('click', () => {
 	document.body.classList.toggle('dark');
 	btnswitch.classList.toggle('active');
+
+	//Guardamos la config en localStorage
+	if (document.body.classList.contains('dark')) {
+		localStorage.setItem('dark-mode', 'true');
+	} else {
+		localStorage.setItem('dark-mode', 'false');
+	}
 });
+
+// Obtengo el modo actual
+if (localStorage.getItem('dark-mode') === 'true') {
+	document.body.classList.add('dark');
+	btnswitch.classList.add('active');
+
+} else {
+	document.body.classList.remove('dark');
+	btnswitch.classList.remove('active');
+
+}
